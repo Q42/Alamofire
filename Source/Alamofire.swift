cb5a256f3bf5204719c26c894b6667671e767be9
Mattt Thompson <m@mattt.me>
2014-10-20 14:20:15 -0700

    Xcode 6.1
    Squashed commit of the following:
    
    commit 6b6facc0ac407bc37e6c71f5d93d11f21894b8ba
    Author: Mattt Thompson <m@mattt.me>
    Date:   Fri Oct 10 05:30:58 2014 -0700
    
        [Issue #164] Fixed mistake in merge
    
    commit 8a814ee8c083faf4e02189665f5b2bc6abfc0650
    Merge: 0930f42 29d8b97
    Author: Mattt Thompson <m@mattt.me>
    Date:   Thu Oct 9 16:01:55 2014 -0700
    
        Merge branch 'xcode-6.1' of github.com:Alamofire/Alamofire into xcode-6.1
    
        Conflicts:
        	Example/DetailViewController.swift
        	Example/MasterViewController.swift
        	Source/Alamofire.swift
        	Tests/ParameterEncodingTests.swift
    
    commit 0930f428eb0b9d142e29a31b94a7af84f48a06e5
    Merge: b1882a1 7362404
    Author: Mattt Thompson <m@mattt.me>
    Date:   Thu Oct 9 15:58:36 2014 -0700
    
        Merge branch 'master' into xcode-6.1
    
        Conflicts:
        	Source/Alamofire.swift
    
    commit 29d8b97bbc588703f5641255e5c2bcf17f58d009
    Merge: 80cc057 18f1d57
    Author: Mattt Thompson <mattt@users.noreply.github.com>
    Date:   Sun Oct 5 09:17:17 2014 -0700
    
        Merge pull request #143 from nduplessis/xcode-6.1
    
        Xcode 6.1 GM fixes
    
    commit 18f1d57fd8a40402c71eb85faba977ee1cd7fe01
    Author: Nico du Plessis <duplessis.nico@gmail.com>
    Date:   Thu Oct 2 13:13:24 2014 +0200
    
        Xcode 6.1 GM fixes
    
    commit 80cc05720013042eccfc8bc20a8304cd3ae656d7
    Merge: 38e0783 f8bb0b4
    Author: Mattt Thompson <mattt@users.noreply.github.com>
    Date:   Tue Sep 30 09:35:22 2014 -0700
    
        Merge pull request #139 from orta/xcode-6.1
    
        @mattt => update to support Xcode 6.1b3
    
    commit f8bb0b4fb8780a5ee74098d09e9a5997f550e425
    Author: Orta Therox <orta.therox@gmail.com>
    Date:   Tue Sep 30 17:17:27 2014 +0100
    
        update to support Xcode 6.1b3
    
    commit b1882a1d64ff4c5de8b514742f0d3a27bcf16bf1
    Merge: 34ee306 bc6a58d
    Author: Mattt Thompson <m@mattt.me>
    Date:   Thu Sep 25 22:17:50 2014 -0700
    
        Merge branch 'master' into xcode-6.1
    
        Conflicts:
        	Source/Alamofire.swift
        	Tests/ParameterEncodingTests.swift
    
    commit 38e07830df5e44b03510c2d74286e82ecebb0513
    Merge: 34ee306 9933340
    Author: Mattt Thompson <mattt@users.noreply.github.com>
    Date:   Fri Sep 19 12:02:23 2014 -0700
    
        Merge pull request #114 from rosskimes/xcode-6.1
    
        Compiles on Xcode 6.1 Beta 2.
    
    commit 99333407f6fa9d7476a39eb9f8f7d1d89e38afe9
    Author: Ross Kimes <ross.kimes@me.com>
    Date:   Fri Sep 19 13:17:02 2014 -0500
    
        Compiles on Xcode 6.1 Beta 2.
    
    commit 34ee3065c98ebd19a5012f3dc03a1b5eb990d51e
    Merge: 67ee13a 6c087a9
    Author: Mattt Thompson <m@mattt.me>
    Date:   Thu Sep 18 07:49:27 2014 -0700
    
        Merge branch 'aral-patch-2' into xcode-6.1
    
    commit 6c087a956ea2947c4de97d3e4b6c13ff06924499
    Merge: 2abe348 d49bbed
    Author: Mattt Thompson <m@mattt.me>
    Date:   Thu Sep 18 07:49:15 2014 -0700
    
        Merge branch 'patch-2' of github.com:aral/Alamofire into aral-patch-2
    
    commit 67ee13aa14baa50f71d2583d2f3b7996224b89ba
    Merge: 2abe348 ab4f4c0
    Author: Mattt Thompson <m@mattt.me>
    Date:   Thu Sep 18 07:48:31 2014 -0700
    
        Merge branch 'patch-1' of github.com:aral/Alamofire into aral-patch-1
    
        Conflicts:
        	Source/Alamofire.swift
    
    commit d49bbeda306850858e5ce189380665eea9eb8ee1
    Author: Aral Balkan <aral@aralbalkan.com>
    Date:   Wed Sep 17 00:33:10 2014 +0100
    
        Replaced toRaw with rawValue (Xcode 6.1 Beta 2 update)
    
    commit ab4f4c0fd83fc7c62018f9f19b4d54d1de107ef9
    Author: Aral Balkan <aral@aralbalkan.com>
    Date:   Wed Sep 17 00:31:15 2014 +0100
    
        Updated fromRaw and toRaw to rawValue
    
        Fixed for Xcode 6.1 Beta 2.

--- Source/Alamofire.swift
+++ Source/Alamofire.swift
@@ -1277,9 +1277,9 @@ extension Request: DebugPrintable {
         }
         
         if let HTTPBody = request.HTTPBody {
-            let escapedBody = NSString(data: HTTPBody, encoding: NSUTF8StringEncoding)
-                                .stringByReplacingOccurrencesOfString("\"", withString: "\\\"")
-            components.append("-d \"\(escapedBody)\"")
+            if let escapedBody = NSString(data: HTTPBody, encoding: NSUTF8StringEncoding)?.stringByReplacingOccurrencesOfString("\"", withString: "\\\"") {
+                components.append("-d \"\(escapedBody)\"")
+            }
         }
 
         components.append("\"\(URL.absoluteString!)\"")
